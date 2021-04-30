1a8f17a317b1fa3b53a3ad9702baa9bc0142b9d7
Lorenzo Carletti <lollo.lollo.rbiz@gmail.com>
2021-04-30 02:39:29 +0200

    Revert "Update to v1.3 tools"
    This reverts commit 444685641b5ae6174914d71083a43c16c288af23.

--- i.bat
+++ i.bat
@@ -3,21 +3,7 @@
 @echo Converting non-script text files to data files
 @echo ---------------------------------------------------------
 @echo.
-@move /Y custom_text.txt custom_text_TMP.txt > NUL
-@move /Y enemynames_short.txt enemynames_short_TMP.txt > NUL
-@move /Y castroll_names.txt castroll_names_TMP.txt > NUL
-@move /Y special_itemdescriptions.txt special_itemdescriptions_TMP.txt > NUL
-@python prepare_special_text.py custom_text_TMP.txt custom_text.txt
-@python prepare_special_text.py enemynames_short_TMP.txt enemynames_short.txt
-@python prepare_special_text.py castroll_names_TMP.txt castroll_names.txt
-@python prepare_special_text.py custom_text_TMP.txt special_itemdescriptions.txt
 @textconv
-@fix_custom_text text_custom_text.bin
-@rearrange_font font_mainfont.bin font_smallfont.bin font_castroll.bin font_mainfont_rearranged.bin font_castroll_rearranged.bin font_mainfont_used.bin font_smallfont_used.bin
-@move /Y custom_text_TMP.txt custom_text.txt > NUL
-@move /Y enemynames_short_TMP.txt enemynames_short.txt > NUL
-@move /Y castroll_names_TMP.txt castroll_names.txt > NUL
-@move /Y special_itemdescriptions_TMP.txt special_itemdescriptions.txt > NUL
 @echo.
 @echo.
 @echo ---------------------------------------------------------
@@ -25,13 +11,6 @@
 @echo ---------------------------------------------------------
 @echo.
 @copy mother3j.gba test.gba
-@echo.
-@echo.
-@echo ---------------------------------------------------------
-@echo Freeing enemy graphics' space
-@echo ---------------------------------------------------------
-@echo.
-@FreeSpace test.gba -v
 @echo.
 @echo.
 @echo ---------------------------------------------------------
@@ -49,13 +28,6 @@
 @echo.
 @echo.
 @echo ---------------------------------------------------------
-@echo Checking overlap of the files that will be used
-@echo ---------------------------------------------------------
-@echo.
-@python check_overlap.py m3hack.asm
-@echo.
-@echo.
-@echo ---------------------------------------------------------
 @echo Compiling .asm files and inserting all new data files
 @echo ---------------------------------------------------------
 @echo.
@@ -64,4 +36,3 @@
 @echo.
 @echo COMPLETE!
 @echo.
-@PAUSE
